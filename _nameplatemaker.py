0407ec6422beddac78dc28f309c803dd31f0e931
Kyle <kael29lv@gmail.com>
2018-05-05 14:03:27 -0700

    Added options dict and lowercase functionality.

--- _nameplatemaker.py
+++ _nameplatemaker.py
@@ -13,9 +13,17 @@ nameplate file.
 from PIL import Image
 
 # TEMP VARIABLES / To be replaced by Preset config import.
-chars = '_components/_chars/_detroit-gray-black/'
+chars = '_components/_chars/_exo-red-black/'
+test_names = ['Doe', 'Zoolander', 'AReallyLongNameThatIsTooLong', 'J. Smith']
 
-test_names = ['Doe', 'Johnson', 'ROBERTS', 'S. Meowman', 'A Very Long Name', 'Evenlongernametotesttheresize']
+
+options = {
+        'kern_offset' : 2,
+        'max_size' : 1000,
+        'force_upper' : False,
+        'char_prefix' : 'chars_'
+        }
+
 
 def resize_image(img, max_w):
     """ Resizes an image to fit the maxsize.
@@ -31,34 +39,67 @@ def resize_image(img, max_w):
     
     return img
 
+
+def crop_to_w_bbox(obj):
+    """ Creates a cropped version of the letter. Crops to the width of the object. """
     
-def create_nameplate(name, charset):
+    # Get the new bounding box, White Transparency Fix
+    im_cords = obj.convert("RGBa").getbbox()
+    obj_w, obj_h = obj.size[0], obj.size[1]
+    
+    # Crop to the sides, but keep the height0
+    im = obj.crop(( im_cords[0], 0, im_cords[2], obj_h))
+    
+    return im
+
+ 
+def create_nameplate(name, charset, **kwargs):
     """ Uses PIL to create a nameplate for a jersey file. 
     
     @str name : Word or name to put together.
     @str charset : Location of the character files to use.
+    @dict **kwargs : Options array that gets unpacked and overrides the defaults.
     
     """
     
     # Some holders
     pil_charset = {}
     widths = []
+       
+    defaults = { 
+            'kern_offset' : 12,     # Spacing between characters
+            'space_offset' : 48,    # Pixels to represent a space
+            'max_size' : 1990,      # Int, max width before it should be flagged.
+            'force_upper' : True,   # Forces to use uppercase.
+            'char_prefix' : 'char_'
+            }
     
-    # Options
-    kern_offset = 12 # Spacing between characters
-    space_offset = 48 # Pixels to represent a space
-    max_size = 1990 # Int, max width before it should be flagged.
+    options = {**defaults, **kwargs} # Merges an options dict with the defaults.
     
+    # Reusable Helpers for the Options
+    kern_offset = options['kern_offset']
+    space_offset = options['space_offset']
+    max_size = options['max_size']
+    force_upper = options['force_upper']
+    char_prefix = options['char_prefix']
+       
     # Get the individual letter components
-    for l in name.lower():
-            
+    for l in name:
+        
+        prefix = char_prefix
+        
+        if force_upper:
+            l = l.upper()
+        elif l.islower():
+            prefix += 'l_'
+        
         if l == ' ':
             print('Space Detected') #DEBUG
         elif l in pil_charset:
             print('Already Found {}, skipping ..'.format(l))
         else:
-            cimg = Image.open(charset + 'char_' + l + '.png')
-            cimg = create_letter(cimg)
+            cimg = Image.open(charset + prefix + l.lower() + '.png')
+            cimg = crop_to_w_bbox(cimg)
             pil_charset[l] = (cimg, cimg.size[0])
             
         # Get the widths
@@ -74,8 +115,11 @@ def create_nameplate(name, charset):
     # Combine them to make the nameplate.
     new_img = Image.new('RGBA', (total_w + kern_total, cimg.size[1]))
     x_offset = 0
-    for l in name.lower():
+    for l in name:
         
+        if force_upper:
+            l = l.upper()
+            
         if l == ' ':
             x_offset +=  space_offset
         else:
@@ -90,20 +134,8 @@ def create_nameplate(name, charset):
     return new_img
 
 
-def create_letter(obj):
-    """ Creates a cropped version of the letter. """
-    
-    # Get the new bounding box, White Transparency Fix
-    im_cords = obj.convert("RGBa").getbbox()
-    obj_w, obj_h = obj.size[0], obj.size[1]
-    
-    # Crop to the sides, but keep the height0
-    im = obj.crop(( im_cords[0], 0, im_cords[2], obj_h))
-    
-    return im
-
-
-for n in test_names:
-    print(n)
-    nameplate = create_nameplate(n, chars)
-    nameplate.save('T_' + n + '.png')
+if __name__ == "__main__":
+    for n in test_names:
+        print(n)
+        nameplate = create_nameplate(n, chars, **options)
+        nameplate.save('T_' + n + '.png')
